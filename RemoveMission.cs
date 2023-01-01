434850e254ac7c7cfd4ab74b2cbca967ed133f98
Makarew <31836770+Makarew@users.noreply.github.com>
2022-12-31 17:53:20 -0700

    Added ToggleMod

--- RemoveMission.cs
+++ RemoveMission.cs
@@ -11,6 +11,18 @@ namespace NeonWhiteQoL
             HarmonyMethod harmonyMethod = new HarmonyMethod(typeof(RemoveMission).GetMethod("PreCreateActionButton"));
             NeonLite.Harmony.Patch(method, harmonyMethod);
         }
+        
+        public static void ToggleMod(int value)
+        {
+            if (value == 0) 
+            {
+                Initialize();
+                return;
+            }
+            
+            MethodInfo method = typeof(MenuScreenLocation).GetMethod("CreateActionButton");
+            NeonLite.Harmony.Unpatch(method, HarmonyPatchType.Prefix);
+        }
 
         public static bool PreCreateActionButton(HubAction hubAction)
         {
