0c56fcb6f1d999359403296fc56e234431b77da2
Makarew <31836770+Makarew@users.noreply.github.com>
2022-12-31 17:34:01 -0700

    Added ToggleMod

--- BegoneApocalypse.cs
+++ BegoneApocalypse.cs
@@ -11,6 +11,18 @@ namespace NeonWhiteQoL
             HarmonyMethod harmonyMethod = new HarmonyMethod(typeof(BegoneApocalypse).GetMethod("RemoveApocalypse"));
             NeonLite.Harmony.Patch(method, null, harmonyMethod);
         }
+        
+        public static void ToggleMod(int value)
+        {
+            if (value == 0) 
+            {
+                Initialize();
+                return;
+            }
+            
+            MethodInfo method = typeof(MenuScreenMapAesthetics).GetMethod("Start", BindingFlags.Instance | BindingFlags.NonPublic);
+            NeonLite.Harmony.Unpatch(method, HarmonyPatchType.Postfix);
+        }
 
         public static void RemoveApocalypse(MenuScreenMapAesthetics __instance)
         {
