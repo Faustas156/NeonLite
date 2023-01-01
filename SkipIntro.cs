56784cbdfbc5e9ee73bcee25a0a54fad5ea28ab2
Makarew <31836770+Makarew@users.noreply.github.com>
2022-12-31 18:00:54 -0700

    Added ToggleMod

--- SkipIntro.cs
+++ SkipIntro.cs
@@ -12,6 +12,18 @@ namespace NeonWhiteQoL
             HarmonyMethod harmonyMethod = new HarmonyMethod(typeof(SkipIntro).GetMethod("SkippingIntro"));
             NeonLite.Harmony.Patch(method, harmonyMethod);
         }
+        
+        public static void ToggleMod(int value)
+        {
+            if (value == 0) 
+            {
+                Initialize();
+                return;
+            }
+            
+            MethodInfo method = typeof(IntroCards).GetMethod("SetState", BindingFlags.NonPublic | BindingFlags.Instance);
+            NeonLite.Harmony.Unpatch(method, HarmonyPatchType.Prefix);
+        }
 
         public static void SkippingIntro(IntroCards __instance)
         {
