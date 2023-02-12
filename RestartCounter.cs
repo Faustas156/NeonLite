9d1eb5fbff6e88424d34a04a4f3d8220d7475d32
faustas <faustastourettes@gmail.com>
2023-02-13 01:20:32 +0200

    removed experimental feature for now

--- RestartCounter.cs
+++ RestartCounter.cs
@@ -4,48 +4,48 @@ using UnityEngine;
 
 namespace NeonWhiteQoL
 {
-    internal class RestartCounter : MonoBehaviour
-    {
-        Game game = Singleton<Game>.Instance;
-        private Dictionary<String, int> dict = new();
+    //internal class RestartCounter : MonoBehaviour
+    //{
+    //    Game game = Singleton<Game>.Instance;
+    //    private Dictionary<String, int> dict = new();
 
-        void Start()
-        {
-            game.OnLevelLoadComplete += OnLevelLoaded;
-            SaveToFile();
-        }
+    //    void Start()
+    //    {
+    //        game.OnLevelLoadComplete += OnLevelLoaded;
+    //        SaveToFile();
+    //    }
 
-        private void OnLevelLoaded()
-        {
-            string currentLevel = game.GetCurrentLevel().levelID;
+    //    private void OnLevelLoaded()
+    //    {
+    //        string currentLevel = game.GetCurrentLevel().levelID;
 
-            if (dict.ContainsKey(currentLevel))
-            {
-                dict[currentLevel] += 1;
-            }
-            else
-            {
-                dict[currentLevel] = 1;
-            }
-            SaveToFile();
-        }
-        private void SaveToFile()
-        {
-            if (!SteamManager.Initialized)
-            {
-                return;
-            }
+    //        if (dict.ContainsKey(currentLevel))
+    //        {
+    //            dict[currentLevel] += 1;
+    //        }
+    //        else
+    //        {
+    //            dict[currentLevel] = 1;
+    //        }
+    //        SaveToFile();
+    //    }
+    //    private void SaveToFile()
+    //    {
+    //        if (!SteamManager.Initialized)
+    //        {
+    //            return;
+    //        }
 
-            string path = Application.persistentDataPath + "\\" + SteamUser.GetSteamID().m_SteamID.ToString() + "\\restartcounter.txt";
-            Stream stream = File.Open(path, FileMode.Create);
+    //        string path = Application.persistentDataPath + "\\" + SteamUser.GetSteamID().m_SteamID.ToString() + "\\restartcounter.txt";
+    //        Stream stream = File.Open(path, FileMode.Create);
 
-            Debug.Log(path);
-            DataContractJsonSerializer serializer = new(typeof(Dictionary<string, int>));
-            serializer.WriteObject(stream, dict);
+    //        Debug.Log(path);
+    //        DataContractJsonSerializer serializer = new(typeof(Dictionary<string, int>));
+    //        serializer.WriteObject(stream, dict);
 
-            stream.Close();
-            Debug.Log("closed");
-        }
+    //        stream.Close();
+    //        Debug.Log("closed");
+    //    }
 
-    }
+    //}
 }
