d213d9a2014cee76cc8e2e724d31606a84b923e3
MOPSKATER <josua.w@gmx.de>
2023-09-16 22:40:37 +0200

    Fixed bug that made all sliders bigger

--- ExtFovSlider.cs
+++ ExtFovSlider.cs
@@ -7,6 +7,10 @@ namespace NeonLite
     {
         [HarmonyPrefix]
         [HarmonyPatch(typeof(SliderUIPrefab), "Initialise")]
-        internal static void Initialize(ref OptionsMenuPanelInformation.OptionEntry optionEntry) => optionEntry.SliderMaximum = 160;
+        internal static void Initialize(ref string localisationKey, ref OptionsMenuPanelInformation.OptionEntry optionEntry)
+        {
+            if (localisationKey == "Interface/OPTIONS_01_FOV")
+                optionEntry.SliderMaximum = 160;
+        }
     }
 }
