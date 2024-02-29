6906ae127778af9f47aaa5cfe4506ad0054dbe8c
MOPSKATER <josua.w@gmx.de>
2024-02-29 19:04:33 +0100

    Readded visual features

--- GameObjects/HUDManager.cs
+++ GameObjects/HUDManager.cs
@@ -1,19 +1,40 @@
 ﻿using ClockStone;
+using HarmonyLib;
 using UnityEngine;
 
 namespace NeonLite.GameObjects
 {
+    [HarmonyPatch]
     internal class HUDManager : MonoBehaviour
     {
         internal static void Initialize()
         {
             AudioController audioController = SingletonMonoBehaviour<AudioController>.Instance;
-            GameObject WhitePortrait = RM.ui.portraitUI.gameObject;
-            GameObject Backstory = WhitePortrait.transform.parent.Find("Backstory").gameObject;
+            GameObject whitePortrait = RM.ui.portraitUI.gameObject;
+            GameObject backstory = whitePortrait.transform.parent.Find("Backstory").gameObject;
+            GameObject bottomBar = RM.ui.transform.Find("Overlays/BottomBar/").gameObject;
 
             audioController.ambienceSoundEnabled = !NeonLite.s_Setting_DisableAmbiance.Value;
-            WhitePortrait.SetActive(!NeonLite.s_Setting_PlayerPortrait.Value);
-            Backstory.SetActive(!NeonLite.s_Setting_BackstoryDisplay.Value);
+            whitePortrait.SetActive(!NeonLite.s_Setting_PlayerPortrait.Value);
+            backstory.SetActive(!NeonLite.s_Setting_BackstoryDisplay.Value);
+            bottomBar.SetActive(!NeonLite.s_Setting_BottombarDisplay.Value);
+            RM.ui.deathOverlay.SetActive(!NeonLite.s_Setting_DamageOverlayDisplay.Value);
         }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerUI), "SetWarningLowHealth")]
+        private static void OverrideSetWarningLowHealth(ref bool on) =>
+            on = !NeonLite.s_Setting_DamageOverlayDisplay.Value && on;
+
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UIScreenFader), "FadeScreen")]
+        private static bool PreFadeScreen(ref UIScreenFader.FadeType ft, ref float time) =>
+            !(NeonLite.s_Setting_ShockerOverlayDisplay.Value && ft == UIScreenFader.FadeType.FadeIn && time == 0.333f);
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerUI), "SetTelefragOverlay")]
+        private static void PreFadeScreen(ref bool on) =>
+            on &= !NeonLite.s_Setting_TelefragOverlayDisplay.Value;
     }
 }
