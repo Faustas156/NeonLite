6b9fa93c56b6d17d45f520459aeffd1575864b0b
st×tic <rmgrichdeveloper@gmail.com>
2024-08-27 18:51:08 -0500

    change noshocker/nostomp to work better
    by request of azurine for her partner

--- Modules/VFX/NoShocker.cs
+++ Modules/VFX/NoShocker.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 
 #pragma warning disable CS0414
 
@@ -14,22 +16,51 @@ namespace NeonLite.Modules.Misc.VFX
 
         static void Setup()
         {
-            var setting = Settings.Add(Settings.h, "VFX", "noShocker", "Disable shocker overlay", "Disable the white flash from shockers (not the explosion!)", false);
+            var setting = Settings.Add(Settings.h, "VFX", "noShocker", "Disable shocker overlay", "Disable the white flash from shockers, including the explosion.", false);
             setting.OnEntryValueChanged.Subscribe((_, after) => Activate(after));
             active = setting.Value;
         }
 
-        static readonly MethodInfo original = AccessTools.Method(typeof(UIScreenFader), "FadeScreen");
+        static readonly MethodInfo original = AccessTools.Method(typeof(ShockWeapon), "DoShock");
         static void Activate(bool activate)
         {
             if (activate)
-                NeonLite.Harmony.Patch(original, prefix: Helpers.HM(StopFade));
+                NeonLite.Harmony.Patch(original, transpiler: Helpers.HM(StopFX));
             else
-                NeonLite.Harmony.Unpatch(original, Helpers.MI(StopFade));
+                NeonLite.Harmony.Unpatch(original, Helpers.MI(StopFX));
 
             active = activate;
         }
 
-        static bool StopFade(UIScreenFader.FadeType ft, float time) => !(ft == UIScreenFader.FadeType.FadeIn && time == 0.333f);
+        static IEnumerable<CodeInstruction> StopFX(IEnumerable<CodeInstruction> instructions)
+        {
+            OpCode stopUntil = OpCodes.Nop;
+            CodeInstruction storage = null;
+            var exploder = AccessTools.Field(typeof(RM), "exploder");
+            var ui = AccessTools.Field(typeof(RM), "ui");
+
+            foreach (var code in instructions)
+            {
+                if (code.LoadsField(exploder))
+                {
+                    storage = code;
+                    stopUntil = OpCodes.Ldstr;
+                }
+                else if (code.LoadsField(ui))
+                {
+                    storage = code;
+                    stopUntil = OpCodes.Ldarg_0;
+                }
+                else if (stopUntil == OpCodes.Nop || stopUntil == code.opcode)
+                {
+                    stopUntil = OpCodes.Nop;
+                    if (storage != null)
+                        yield return code.MoveLabelsFrom(storage);
+                    else
+                        yield return code;
+                    storage = null;
+                }
+            }
+        }
     }
 }
--- Modules/VFX/NoStomp.cs
+++ Modules/VFX/NoStomp.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 using UnityEngine.Rendering;
 
 namespace NeonLite.Modules.VFX
@@ -15,22 +17,51 @@ namespace NeonLite.Modules.VFX
 
         static void Setup()
         {
-            var setting = Settings.Add(Settings.h, "VFX", "noStomp", "Disable stomp splashbang", "Disable the white flash that appears when you land a stomp.", false);
+            var setting = Settings.Add(Settings.h, "VFX", "noStomp", "Disable stomp splashbang", "Disable the white flash and explosion that appear when you land a stomp.", false);
             setting.OnEntryValueChanged.Subscribe((_, after) => Activate(after));
             active = setting.Value;
         }
 
-        static readonly MethodInfo original = AccessTools.Method(typeof(ScannerEffect), "OnStomp");
+        static readonly MethodInfo original = AccessTools.Method(typeof(MechController), "DoStompAbility");
         static void Activate(bool activate)
         {
             if (activate)
-                NeonLite.Harmony.Patch(original, prefix: Helpers.HM(StopFade));
+                NeonLite.Harmony.Patch(original, transpiler: Helpers.HM(StopFX));
             else
-                NeonLite.Harmony.Unpatch(original, Helpers.MI(StopFade));
+                NeonLite.Harmony.Unpatch(original, Helpers.MI(StopFX));
 
             active = activate;
         }
 
-        static bool StopFade() => false;
+        static IEnumerable<CodeInstruction> StopFX(IEnumerable<CodeInstruction> instructions)
+        {
+            OpCode stopUntil = OpCodes.Nop;
+            CodeInstruction storage = null;
+            var exploder = AccessTools.Field(typeof(RM), "exploder");
+            var scannerEffect = AccessTools.Field(typeof(RM), "scannerEffect");
+
+            foreach (var code in instructions)
+            {
+                if (code.LoadsField(exploder))
+                {
+                    storage = code;
+                    stopUntil = OpCodes.Ldstr;
+                }
+                else if (code.LoadsField(scannerEffect))
+                {
+                    storage = code;
+                    stopUntil = OpCodes.Ldarg_0;
+                }
+                else if (stopUntil == OpCodes.Nop || stopUntil == code.opcode)
+                {
+                    stopUntil = OpCodes.Nop;
+                    if (storage != null)
+                        yield return code.MoveLabelsFrom(storage);
+                    else
+                        yield return code;
+                    storage = null;
+                }
+            }
+        }
     }
 }
